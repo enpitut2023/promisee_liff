bda2380e4e2fd241bef1fcaa864a4bdebaecf0ea
kou7306 <kouta.1206.y@icloud.com>
2024-01-10 18:26:06 +0900

    default-value

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def index():
 def gifts():
     # クエリパラメータを取得
     min_price = request.args.get('min_price', default=0, type=int)
-    max_price = request.args.get('max_price', default=1000, type=int)
-    if min_price==0 and max_price==1000:
+    max_price = request.args.get('max_price', default=9999, type=int)
+    if min_price==0 and max_price==9999:
         return render_template('gift_first.html')
 
     gifts_data = db.collection('gifts').get() # ギフトデータ取得
